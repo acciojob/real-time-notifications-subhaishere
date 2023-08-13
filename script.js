86ef498c1bcc74cba3d938a61010ea7d1d2d775d
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+// complete the code
+const statusDiv = document.getElementById("status");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+const disconnectButton = document.getElementById("disconnect-button");
+const notificationsDiv = document.getElementById("notifications");
+const wsUrl = "wss://socketsbay.com/wss/v2/1/demo/";
+let socket;
+
+const displayNotification = (message) => {};
+
+const connectWebSocket = () => {};
+
+sendButton.addEventListener("click", () => {});
+
+disconnectButton.addEventListener("click", () => {});
+
+connectWebSocket();
